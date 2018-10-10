d560f7fcf3cc7ac5fe8794878f52ada9429e3d89
stamo15 <stamo15@alustudent.com>
2018-10-10 09:41:19 +0200

    Updated the colors to match the original flags

--- flags.py
+++ flags.py
@@ -9,13 +9,13 @@ window_height = 600
 def benin():
     enable_fill()
     #Vertical green rectangle
-    set_fill_color(0, 1, 0)
+    set_fill_color(0, 0.53, 0.32)
     draw_rectangle(0, 0, window_width//3, window_height)
     #Horizontal yellow rectangle
-    set_fill_color(1, 1, 0)
+    set_fill_color(0.98, 0.82, 0.08)
     draw_rectangle(window_width//3, 0, window_width - window_width//3, window_height//2)
     #Horizontal red rectangle
-    set_fill_color(1, 0, 0)
+    set_fill_color(0.9, 0.06, 0.17)
     draw_rectangle(window_width//3, window_height//2, window_width - window_width//3, window_height//2)
 
 
@@ -24,25 +24,25 @@ def benin():
 def niger():
     enable_fill()
     #First horizontal orange rectangle
-    set_fill_color(1, .5, 0)
+    set_fill_color(.87, .32, .02)
     draw_rectangle(0, 0, window_width, window_height // 3)
     #White horizontal rectangle with orange circle in its middle
-    set_fill_color(1, .5, 0)
+    set_fill_color(.87, .32, .02)
     draw_circle(window_width//2, window_height//2, window_height//6 - 10)
     #Green rectangle
-    set_fill_color(0, 1, 0)
+    set_fill_color(.05, .7, .16)
     draw_rectangle(0, 2 * window_height//3, window_width, window_height // 3)
 
 def guinea():
     enable_fill()
     # Vertical red rectangle
-    set_fill_color(1, 0, 0)
+    set_fill_color(.8, .06, .15)
     draw_rectangle(0, 0, window_width // 3, window_height)
     # Vertical yellow rectangle
-    set_fill_color(1, 1, 0)
+    set_fill_color(.98, .82, .08)
     draw_rectangle(window_width // 3, 0, window_width // 3, window_height)
     # Vertical green rectangle
-    set_fill_color(0, 1, 0)
+    set_fill_color(0, .58, .37)
     draw_rectangle(2 * window_width // 3, 0, window_width // 3, window_height)
 
 
